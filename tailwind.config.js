7be53f4a2203aa1019a52e9e9fe6f1dd2dada4b0
Ayush Soni <ayushsoni1010.work@gmail.com>
2022-06-12 15:23:58 +0530

    chore(config): tailwindcss installation and configuration changes

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    spacing: {
+      1: "8px",
+      2: "8px",
+      3: "8px",
+    },
+    screens: {
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+      xxl: "1536px",
+    },
+    extend: {},
+  },
+  prefix:"tw-",
+  plugins: [],
+};

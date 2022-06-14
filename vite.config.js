3235225620551fffc480dc0c7794d539b56537f4
Ayush Soni <ayushsoni1010.work@gmail.com>
2022-06-14 23:15:36 +0530

    update(config): vite and cypress configuration changes

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// vite --config my-config.js
 // https://vitejs.dev/config/
-
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./src/setup.js"],
+  },
 });
